c3f7f8bc9e5b59cb85c2f40cc39575d40a4009e1
Zach Goodson <58115931+Quantam-Studios@users.noreply.github.com>
2022-05-21 15:10:43 -0500

    Excel Functionality.
    Added basic excel usage. Implemented updates made by collaborators.

--- cchs_football_stats/lib/main.dart
+++ cchs_football_stats/lib/main.dart
@@ -1,4 +1,15 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:cchs_football_stats/models/game.dart';
+import 'package:cchs_football_stats/models/combo.dart';
+import 'package:excel/excel.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:cchs_football_stats/models/season.dart';
 import 'package:flutter/material.dart';
+import './models/game.dart';
+import './models/season.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -44,6 +55,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Season> seasons = <Season>[];
+  List<String> theLetters = <String>[];
+
+  int getCurrentSeason() {
+    if (seasons.length != 0) {
+      for (int x = seasons.length - 1; x < 0; x--) {
+        String year = seasons[x].years;
+        if (year == DateTime.now().year.toString()) {
+          return x;
+        }
+      }
+      return -2;
+    } else {
+      return -1;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,9 +83,74 @@ class _MyHomePageState extends State<MyHomePage> {
           widget.title,
           style: const TextStyle(color: Colors.white),
         ),
+        leading: IconButton(
+            onPressed: () {
+              pickFile();
+            },
+            icon: const Text("+",
+                style: TextStyle(
+                    color: Color.fromARGB(192, 224, 9, 9), fontSize: 30))),
       ),
 
-      //
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
+  }
+
+  pickFile() async {
+    FilePickerResult? file = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['xlsx', 'xls'],
+      withData: true,
+    );
+    //withReadStream: true); // makes a file picker window
+    if (file == null) return; // checks if you selected a file
+
+    Directory directory = Directory("This PC/Documents/football games");
+
+    print(directory.path.isEmpty);
+
+    PlatformFile theFile = file.files.single;
+    Uint8List? theByteCode = theFile.bytes;
+
+    //print(String.fromCharCode(80));
+
+    //int theString = theByteCode.forEach((element) {convert(element);});
+    //theByteCode.remove();
+    //theByteCode.forEach( => (int));
+    //print("this is the byte code");
+    //print(theFile.bytes);
+    String theFileName = theFile.name;
+    Game game = Game(); // makes a new game
+    game.combos = [];
+    int currentSeasonIndex = getCurrentSeason();
+    if (currentSeasonIndex == -1) {
+      Season season = Season();
+      season.years = DateTime.now().year.toString();
+      seasons.add(season);
+      currentSeasonIndex = seasons.length - 1;
+      season.games = [];
+    }
+
+    seasons[currentSeasonIndex].games.add(game);
+    int currentGameIndex = seasons[currentSeasonIndex].games.indexOf(game);
+    //print(seasons);
+    if (theByteCode != null) {
+      Excel excelFile = Excel.decodeBytes(theByteCode);
+      List<List<Data?>> excelSheet = excelFile.sheets.values.single.rows;
+      excelSheet.forEach(
+          (element) => makePlay(element, currentGameIndex, currentSeasonIndex));
+      //print(excelFile.toString());
+      print(excelSheet.toString());
+    } else {
+      print("the byte code is null");
+    }
+  }
+
+  void makePlay(List<Data?> data, int gameIndex, int seasonIndex) {
+    Combo combo = Combo();
+    seasons[seasonIndex].games[gameIndex].combos.add(combo);
   }
 }
